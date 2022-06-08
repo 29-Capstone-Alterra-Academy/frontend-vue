772247df4ab78e9c1399fa6f98b165af5f1b4062
anindyadeffanabila@gmail.com <66814960+anindyadeffa@users.noreply.github.com>
2022-06-08 18:41:59 +0700

    Updated home page

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,9 +1,37 @@
 <template>
-  <v-row justify="center" style="position: relative">
+  <v-row justify="center" style="position: relative;">
     <v-col cols="7">
       <v-row>
+        <v-col cols="12">
+          <v-card class="rounded-lg mx-auto" outlined>
+            <v-row class="pt-4 pb-0 mr-1 pl-2">
+              <v-col cols="11">
+                <v-text-field label="New Post" placeholder="Create New Post" outlined dense></v-text-field>
+              </v-col>
+              <v-col cols="1">
+                <div class="text-center mt-1">
+                  <v-icon>mdi-image-plus</v-icon>
+                </div>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+        <v-col cols="12">
+          <v-card class="rounded-lg mx-auto py-3 px-3" outlined>
+            <v-row>
+              <v-col cols="auto">
+                <v-btn outlined class="text-capitalize">
+                  <v-icon>mdi-fire</v-icon>Rekomendasi
+                </v-btn>
+              </v-col>
+              <v-col cols="auto">
+                <v-btn outlined class="text-capitalize">Mengikuti</v-btn>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
         <v-col v-for="thread in threads" :key="thread.id" cols="12">
-          <v-card class="mx-auto" outlined>
+          <v-card class="rounded-lg mx-auto" outlined>
             <v-list-item three-line>
               <v-list-item-content>
                 <v-row>
@@ -11,16 +39,21 @@
                     <v-img
                       :src="thread.user.avatar"
                       class="image-rounded"
-                      width="30"
+                      width="35"
                     ></v-img>
                   </v-col>
-                  <v-col>
-                    <div class="text-overline mb-4">
+                  <v-col align-self="center" cols="auto">
+                    <div>
                       {{ thread.user.username }}
                     </div>
                   </v-col>
+                  <v-col align-self="center">
+                    <div>
+                      <small class="text--disabled">Posted by @{{thread.user.username}} {{thread.user.timepost}} ago</small>
+                    </div>
+                  </v-col>
                 </v-row>
-                <v-list-item-title class="text-h5 mb-1">
+                <v-list-item-title class="text-h6 my-2">
                   {{ thread.slug }}
                 </v-list-item-title>
                 <v-flex class="text-center">
@@ -39,7 +72,7 @@
                   <v-col cols="5" class="pa-0">
                     <v-row>
                       <v-col justify="center">
-                        <v-icon>mdi-thumb-up</v-icon>
+                        <v-icon>mdi-heart</v-icon>
                         <p class="ma-0" style="width: 50%; display: inline">{{ thread.likes }}</p>
                       </v-col>
                       <v-col justify="center">
@@ -58,19 +91,20 @@
         </v-col>
       </v-row>
     </v-col>
-    <v-col cols="5" style="width: 493.75px">
+    <v-col cols="4" style="width: 373.75px">
       <v-row style="position: fixed; width: inherit">
         <v-col cols="12">
-          <v-card outlined>
+          <v-card class="rounded-lg" outlined>
             <v-list-item three-line>
               <v-list-item-content>
+                <h4 class="text-overline">Hot Topic</h4>
                 <v-row>
                   <v-col v-for="topic in topics" :key="topic.id" cols="12">
                     <v-row>
                       <v-col cols="1" class="pr-0" style="max-width: 2rem">
                         <div class="text-overline mb-4">{{ topic.id }}.</div>
                       </v-col>
-                      <v-col cols="1" class="px-0" style="max-width: none">
+                      <v-col cols="1" class="px-2" style="max-width: none">
                         <v-img
                           :src="topic.image"
                           class="image-rounded"
@@ -83,7 +117,7 @@
                         </div>
                       </v-col>
                       <v-col cols="2" style="max-width: 10rem">
-                        <v-btn> Follow </v-btn>
+                        <v-btn class="text-capitalize">Follow</v-btn>
                       </v-col>
                     </v-row>
                   </v-col>
@@ -93,31 +127,21 @@
           </v-card>
         </v-col>
         <v-col cols="12">
-          <v-card outlined>
+          <v-card class="rounded-lg" outlined>
             <v-list-item three-line>
               <v-list-item-content>
                 <v-row>
-                  <v-col v-for="topic in topics" :key="topic.id" cols="12">
-                    <v-row>
-                      <v-col cols="1" class="pr-0" style="max-width: 2rem">
-                        <div class="text-overline mb-4">{{ topic.id }}.</div>
-                      </v-col>
-                      <v-col cols="1" class="px-0" style="max-width: none">
-                        <v-img
-                          :src="topic.image"
-                          class="image-rounded"
-                          width="30"
-                        ></v-img>
-                      </v-col>
-                      <v-col class="pl-0">
-                        <div class="text-overline mb-4">
-                          {{ topic.name }}
-                        </div>
-                      </v-col>
-                      <v-col cols="3" style="max-width: 10rem">
-                        <v-btn> Follow </v-btn>
-                      </v-col>
-                    </v-row>
+                  <v-col cols="12">
+                    <v-btn outlined block small class="rounded-lg text-capitalize text-left p-2">
+                      <v-icon>mdi-plus</v-icon>
+                      Create New Post
+                    </v-btn>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-btn outlined block small class="rounded-lg text-capitalize text-left p-2">
+                      <v-icon>mdi-plus</v-icon>
+                      Create New Topic
+                    </v-btn>
                   </v-col>
                 </v-row>
               </v-list-item-content>
@@ -141,6 +165,7 @@ export default {
           user: {
             username: 'JohnLeider',
             avatar: 'https://randomuser.me/api/portraits/women/84.jpg',
+            timepost: '8 hours'
           },
           body: 'This is dummy post`s body',
           image: 'https://cataas.com/cat',
@@ -171,14 +196,20 @@ export default {
               username: 'JohnLeider',
               body: 'This is dummy post`s comment',
             },
+            {
+              id: 6,
+              username: 'JohnLeider',
+              body: 'This is dummy post`s comment',
+            },
           ],
         },
         {
           id: 2,
           slug: 'This is dummy post`s slug',
           user: {
-            username: 'JohnLeider',
-            avatar: 'https://randomuser.me/api/portraits/women/84.jpg',
+            username: 'DianaPetersen',
+            avatar: 'https://randomuser.me/api/portraits/women/85.jpg',
+            timepost: '10 hours'
           },
           body: 'This is dummy post`s body',
           image: null,
@@ -197,6 +228,7 @@ export default {
           user: {
             username: 'JohnLeider',
             avatar: 'https://randomuser.me/api/portraits/women/84.jpg',
+            timepost: '18 hours'
           },
           body: 'This is dummy post`s body',
           image: 'https://cataas.com/cat',
@@ -209,6 +241,7 @@ export default {
           user: {
             username: 'JohnLeider',
             avatar: 'https://randomuser.me/api/portraits/women/84.jpg',
+            timepost: '1 months'
           },
           body: 'This is dummy post`s body',
           image: null,
@@ -227,6 +260,7 @@ export default {
           user: {
             username: 'JohnLeider',
             avatar: 'https://randomuser.me/api/portraits/women/84.jpg',
+            timepost: '1 hours'
           },
           body: 'This is dummy post`s body',
           image: 'https://cataas.com/cat',
@@ -245,6 +279,7 @@ export default {
           user: {
             username: 'JohnLeider',
             avatar: 'https://randomuser.me/api/portraits/women/84.jpg',
+            timepost: '5 hours'
           },
           body: 'This is dummy post`s body',
           image: null,
@@ -263,6 +298,7 @@ export default {
           user: {
             username: 'JohnLeider',
             avatar: 'https://randomuser.me/api/portraits/women/84.jpg',
+            timepost: '8 hours'
           },
           body: 'This is dummy post`s body',
           image: 'https://cataas.com/cat',
@@ -291,6 +327,7 @@ export default {
           user: {
             username: 'JohnLeider',
             avatar: 'https://randomuser.me/api/portraits/women/84.jpg',
+            timepost: '6 hours'
           },
           body: 'This is dummy post`s body',
           image: null,
@@ -307,12 +344,12 @@ export default {
       topics: [
         {
           id: 1,
-          name: 'Genshin Impact',
+          name: 'Technology',
           image: 'https://randomuser.me/api/portraits/women/84.jpg',
         },
         {
           id: 2,
-          name: 'Cing Kucing',
+          name: 'Kucing',
           image: 'https://randomuser.me/api/portraits/women/84.jpg',
         },
       ],
