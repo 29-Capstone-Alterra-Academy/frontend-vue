13a0b61a8f2a7e460606d54af4194e442ecf3756
anindyadeffanabila@gmail.com <66814960+anindyadeffa@users.noreply.github.com>
2022-06-28 22:08:37 +0700

    Create topic and user list admin

---
+++ pages/dashboard/topic-list.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" class="my-2">
+        <h2>Topic Lists</h2>
+        <p>Lists to all of the topic that users made</p>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="12">
+        <v-card>
+          <v-card-title>
+            Topic List
+            <v-spacer></v-spacer>
+            <v-text-field
+              v-model="search"
+              append-icon="mdi-magnify"
+              label="Search"
+              single-line
+              hide-details
+            ></v-text-field>
+          </v-card-title>
+          <v-data-table :headers="headers" :search="search" :items="topics">
+            <template #[`item.followers`]="{ item }">
+              <span>{{ item.followers | abbr }}</span>
+            </template>
+            <template #[`item.created_at`]="{ item }">
+              <span>{{ new Date(item.created_at).toLocaleString() }}</span>
+            </template>
+            <template #[`item.details`]="{ item }">
+              <v-btn class="mx-2" @click="detailsItem(item)"> Details </v-btn>
+            </template>
+          </v-data-table>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'TopicList',
+  filters: {
+    abbr(num) {
+      if (String(num).length < 7) {
+        return Math.floor(num / 1000) + 'k'
+      } else {
+        return Math.floor(num / 1000000) + 'm'
+      }
+    },
+  },
+  data() {
+    return {
+      search: '',
+      headers: [
+        {
+          text: 'Topic Name',
+          align: 'start',
+          sortable: false,
+          value: 'name',
+        },
+        { text: 'Date Created', value: 'created_at', align: 'center' },
+        { text: 'Total Post', value: 'total_post', align: 'center' },
+        { text: 'Followers', value: 'followers', align: 'center' },
+        { text: 'Actions', value: 'details', align: 'center', sortable: false },
+      ],
+    }
+  },
+  computed: {
+    topics() {
+      // if (this.searchPost) {
+      //   return this.$store.state.topics.lists.filter((item) => {
+      //     return item.name.toLowerCase().includes(this.searchPost.toLowerCase())
+      //   })
+      // }
+      return this.$store.state.lists.topics
+    },
+  },
+  created() {
+    this.$store.dispatch('lists/fetchTopics')
+  },
+}
+</script>
+
+<style scoped></style>
---
+++ pages/dashboard/user-list.vue
@@ -0,0 +1,100 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" class="my-2">
+        <h2>User List</h2>
+        <p>Lists to all of registered Users</p>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="12">
+        <v-card>
+          <v-card-title>
+            User List
+            <v-spacer></v-spacer>
+            <v-text-field
+              v-model="search"
+              append-icon="mdi-magnify"
+              label="Search"
+              single-line
+              hide-details
+            ></v-text-field>
+          </v-card-title>
+          <v-data-table
+            :headers="headers"
+            :items="users"
+            :search="search"
+            class="elevation-1"
+          >
+            <template #[`item.username`]="{ item }">
+              <div class="d-flex">
+                <v-img
+                  :src="item.profile_image"
+                  max-width="25"
+                  class="rounded-circle"
+                ></v-img>
+                <span class="px-2">{{ item.username }}</span>
+              </div>
+            </template>
+            <template #[`item.created_at`]="{ item }">
+              <span>{{ new Date(item.created_at).toLocaleString() }}</span>
+            </template>
+            <template #[`item.followers`]="{ item }">
+              <span>{{ item.followers | abbr }}</span>
+            </template>
+            <template #[`item.status`]="{ item }">
+              <v-chip color="green" outlined>
+                {{ item.status }}
+              </v-chip>
+            </template>
+          </v-data-table>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'UserList',
+  filters: {
+    abbr(num) {
+      if (String(num).length < 7) {
+        return Math.floor(num / 1000) + 'k'
+      } else {
+        return Math.floor(num / 1000000) + 'm'
+      }
+    },
+  },
+  data() {
+    return {
+      search: '',
+      headers: [
+        {
+          text: 'User',
+          align: 'start',
+          sortable: false,
+          value: 'username',
+        },
+        { text: 'Status', align: 'center', value: 'status' },
+        { text: 'Post Created', align: 'center', value: 'created_at' },
+        { text: 'Followers', align: 'center', value: 'followers' },
+        { text: 'Likes Received', align: 'center', value: 'likes' },
+      ],
+    }
+  },
+  computed: {
+    users() {
+      // return this.$store.state.lists.users.filter((item) => {
+      //   return item.username
+      //     .toLowerCase()
+      //     .includes(this.$route.params.slug.toLowerCase())
+      // })
+      return this.$store.state.lists.users
+    },
+  },
+  created() {
+    this.$store.dispatch('lists/fetchUsers')
+  },
+}
+</script>
